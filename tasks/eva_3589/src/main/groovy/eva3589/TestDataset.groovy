b7b717cba0b94bc149be5ec81cf9f12f051d2863
April Shen <ashen@ebi.ac.uk>
2024-06-14 15:10:03 +0100

    implement data copy method

--- tasks/eva_3589/src/main/groovy/eva3589/TestDataset.groovy
+++ tasks/eva_3589/src/main/groovy/eva3589/TestDataset.groovy
@@ -7,8 +7,7 @@ import uk.ac.ebi.eva.groovy.commons.EVADatabaseEnvironment
 
 import static org.springframework.data.mongodb.core.query.Criteria.where
 import static org.springframework.data.mongodb.core.query.Query.query
-import static uk.ac.ebi.eva.groovy.commons.EVADatabaseEnvironment.getDbsnpSveClass
-import static uk.ac.ebi.eva.groovy.commons.EVADatabaseEnvironment.getSveClass
+import static uk.ac.ebi.eva.groovy.commons.EVADatabaseEnvironment.*
 
 class TestDataset {
     static def logger = LoggerFactory.getLogger(TestDataset.class)
@@ -34,7 +33,48 @@ class TestDataset {
     }
 
     def copyDataToDev(List<Long> rsids) {
-        //TODO
+        // Copy the following data to from prod to dev:
+        // 1. all CVEs with the specified RS IDs
+        // 2. all SVEs assigned to the specified RS IDs
+        // 3. all CVOEs involving those CVEs found in (1)
+        // 4. all SVOEs involving those SVEs found in (2)
+        def cvesToCopy = prodEnv.mongoTemplate.find(query(where("asm").is(assembly).and("accession").in(rsids)), cveClass)
+        logger.info("Copying ${cvesToCopy.size()} CVEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(cvesToCopy, cveClass)
+
+        def dbsnpCvesToCopy = prodEnv.mongoTemplate.find(query(where("asm").is(assembly).and("accession").in(rsids)), dbsnpCveClass)
+        logger.info("Copying ${dbsnpCvesToCopy.size()} dbSNP CVEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(dbsnpCvesToCopy, dbsnpCveClass)
+
+        def svesToCopy = prodEnv.mongoTemplate.find(query(where("seq").is(assembly).and("rs").in(rsids)), sveClass)
+        logger.info("Copying ${svesToCopy.size()} SVEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(svesToCopy, sveClass)
+
+        def dbsnpSvesToCopy = prodEnv.mongoTemplate.find(query(where("seq").is(assembly).and("rs").in(rsids)), dbsnpSveClass)
+        logger.info("Copying ${dbsnpSvesToCopy.size()} dbSNP SVEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(dbsnpSvesToCopy, dbsnpSveClass)
+
+        def cvoesToCopy = prodEnv.mongoTemplate.find(query(where("inactiveObjects.asm").is(assembly)
+                .orOperator(where("accession").in(rsids), where("inactiveObjects.accession").in(rsids))), cvoeClass)
+        logger.info("Copying ${cvoesToCopy.size()} CVOEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(cvoesToCopy, cvoeClass)
+
+        def dbsnpCvoesToCopy = prodEnv.mongoTemplate.find(query(where("inactiveObjects.asm").is(assembly)
+                .orOperator(where("accession").in(rsids), where("inactiveObjects.accession").in(rsids))), dbsnpCvoeClass)
+        logger.info("Copying ${dbsnpCvoesToCopy.size()} dbSNP CVOEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(dbsnpCvoesToCopy, dbsnpCvoeClass)
+
+        def ssids = [svesToCopy, dbsnpSvesToCopy].collectMany{ it.accession }
+
+        def svoesToCopy = prodEnv.mongoTemplate.find(query(where("inactiveObjects.seq").is(assembly)
+                .orOperator(where("accession").in(ssids), where("inactiveObjects.accession").in(ssids))), svoeClass)
+        logger.info("Copying ${svoesToCopy.size()} SVOEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(svoesToCopy, svoeClass)
+
+        def dbsnpSvoesToCopy = prodEnv.mongoTemplate.find(query(where("inactiveObjects.seq").is(assembly)
+                .orOperator(where("accession").in(ssids), where("inactiveObjects.accession").in(ssids))), dbsnpSvoeClass)
+        logger.info("Copying ${dbsnpSvoesToCopy.size()} dbSNP SVOEs to dev...")
+        devEnv.bulkInsertIgnoreDuplicates(dbsnpSvoesToCopy, dbsnpSvoeClass)
     }
 
     def backupDevCollections() {
