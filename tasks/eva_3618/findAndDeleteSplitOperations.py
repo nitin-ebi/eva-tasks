e4ee989e93ca351b8d54332060a86e003048226e
nkumar2 <nkumar2@ebi.ac.uk>
2024-08-14 12:16:40 +0100

    review comment - save docs in the file

--- tasks/eva_3618/findAndDeleteSplitOperations.py
+++ tasks/eva_3618/findAndDeleteSplitOperations.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import logging
+import os
 
 from ebi_eva_common_pyutils.logger import logging_config as log_cfg
 from ebi_eva_internal_pyutils.metadata_utils import get_metadata_connection_handle
@@ -26,10 +28,12 @@ def get_list_of_assemblies_to_check(private_config_xml_file):
     return asm_list
 
 
-def find_and_delete_split_operations(private_config_xml_file):
+def find_and_delete_split_operations(private_config_xml_file, working_dir):
     asm_list = get_list_of_assemblies_to_check(private_config_xml_file)
     logger.info(f'List of asm to search: {asm_list}')
 
+    cve_docs = []
+    sve_docs = []
     cve_ops_ids = set()
     cve_ops_accs = set()
     sve_ops_ids = set()
@@ -46,11 +50,15 @@ def find_and_delete_split_operations(private_config_xml_file):
             cursor = cve_coll.find(cve_query, no_cursor_timeout=True)
 
             for cve_ops_doc in cursor:
+                cve_docs.append(cve_ops_doc)
                 cve_ops_ids.add(cve_ops_doc['_id'])
                 cve_ops_accs.add(cve_ops_doc['accession'])
 
-        logger.info(f'CVE OPS IDS : {cve_ops_ids}')
-        logger.info(f'CVE OPS ACCS : {cve_ops_accs}')
+        logger.info(f'Number of CVE documents: {len(cve_docs)}')
+        # Save CVE documents to file
+        cve_docs_file = os.path.join(working_dir, "cve_docs.json")
+        with open(cve_docs_file, 'w') as f:
+            json.dump(cve_docs, f, default=str)
 
         # get sve ops ids
         for collection in SVE_OPS_COLLECTIONS:
@@ -64,29 +72,35 @@ def find_and_delete_split_operations(private_config_xml_file):
                 rs = f"rs{sve_ops_doc['inactiveObjects'][0]['rs']}"
                 reason = f'SS was associated with the split RS {rs} that was split from {rs} after remapping.'
                 if sve_ops_doc['reason'] == reason:
+                    sve_docs.append(sve_ops_doc)
                     sve_ops_ids.add(sve_ops_doc['_id'])
 
-        logger.info(f'SVE OPS IDS : {sve_ops_ids}')
+        logger.info(f'Number of SVE documents: {len(sve_docs)}')
+        # Save SVE documents to file
+        sve_docs_file = os.path.join(working_dir, "sve_docs.json")
+        with open(sve_docs_file, 'w') as f:
+            json.dump(sve_docs, f, default=str)
 
         # delete sve operations
-        sve_del_query = {'_id': {'$in': list(sve_ops_ids)}}
-        sve_coll = mongo_conn['eva_accession_sharded']['submittedVariantOperationEntity']
-        sve_coll.delete_many(sve_del_query, no_cursor_timeout=True)
-        dbsnp_sve_coll = mongo_conn['eva_accession_sharded']['dbsnpSubmittedVariantOperationEntity']
-        dbsnp_sve_coll.delete_many(sve_del_query)
+        for collection in SVE_OPS_COLLECTIONS:
+            sve_coll = mongo_conn['eva_accession_sharded'][collection]
+            sve_del_query = {'_id': {'$in': list(sve_ops_ids)}}
+            result = sve_coll.delete_many(sve_del_query)
+            logger.info(f'Deleted {result.deleted_count} SVE documents from {collection}')
 
         # delete cve operations
-        cve_del_query = {'_id': {'$in': list(cve_ops_ids)}}
-        cve_coll = mongo_conn['eva_accession_sharded']['clusteredVariantOperationEntity']
-        cve_coll.delete_many(cve_del_query, no_cursor_timeout=True)
-        dbsnp_cve_coll = mongo_conn['eva_accession_sharded']['dbsnpClusteredVariantOperationEntity']
-        dbsnp_cve_coll.delete_many(cve_del_query, no_cursor_timeout=True)
+        for collection in CVE_OPS_COLLECTIONS:
+            cve_coll = mongo_conn['eva_accession_sharded'][collection]
+            cve_del_query = {'_id': {'$in': list(cve_ops_ids)}}
+            result = cve_coll.delete_many(cve_del_query)
+            logger.info(f'Deleted {result.deleted_count} CVE documents from {collection}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find and delete RS_SPLIT operations that were split into the same RS',
                                      add_help=False)
     parser.add_argument("--private-config-xml-file", help="ex: /path/to/eva-maven-settings.xml", required=True)
+    parser.add_argument("--working-dir", help="ex: /path/to/dir where ", required=True)
     args = parser.parse_args()
 
-    find_and_delete_split_operations(args.private_config_xml_file)
+    find_and_delete_split_operations(args.private_config_xml_file, args.working_dir)
