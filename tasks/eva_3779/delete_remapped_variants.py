c813860ea19e40888b0b1b90788155c7a70c8962
tcezard <tcezard@ebi.ac.uk>
2025-04-03 13:17:22 +0100

    New script to delete variants that were remapped

---
+++ tasks/eva_3779/delete_remapped_variants.py
@@ -0,0 +1,80 @@
+import sys
+from argparse import ArgumentParser
+from itertools import islice
+
+from ebi_eva_common_pyutils.logger import logging_config as log_cfg
+from ebi_eva_internal_pyutils.mongo_utils import get_mongo_connection_handle
+
+logger = log_cfg.get_logger(__name__)
+
+
+assembly_to_delete = 'GCA_002263795.4'
+
+def chunked_iterable(iterable, size):
+    iterator = iter(iterable)
+    while chunk := list(islice(iterator, size)):
+        yield chunk
+
+
+def delete_variants(private_config_xml_file, profile, source_collection, chunk_size = 1000):
+    with get_mongo_connection_handle(profile, private_config_xml_file) as mongo_conn:
+        db_name = 'eva_accession_sharded'
+        sve_find_filter = {"seq": assembly_to_delete, 'remappedFrom':{'$exists':1}}
+        operation_collections = ['submittedVariantOperationEntity', 'dbsnpSubmittedVariantOperationEntity']
+        cluster_collections = ['clusteredVariantEntity', 'dbsnpClusteredVariantEntity']
+        submitted_collections = ['submittedVariantEntity', 'dbsnpSubmittedVariantEntity']
+        sve_coll = mongo_conn[db_name][source_collection]
+        cursor = sve_coll.find(sve_find_filter)
+
+        chunk_num = 1
+        for variants_chunk in chunked_iterable(cursor, chunk_size):
+            logger.info(f"Processing chunk {chunk_num}")
+            chunk_num += 1
+            hash_to_delete = [variant.get('_id') for variant in variants_chunk]
+            sve_accessions = [variant.get('accession') for variant in variants_chunk]
+            cve_accessions = set([variant.get('rs') for variant in variants_chunk if variant.get('rs')])
+
+            # delete SVE
+            delete_result = sve_coll.delete_many({'_id': {'$in': hash_to_delete}})
+            logger.info(f"Deleted {delete_result.deleted_count} documents from {collection}.")
+
+            # Delete submitted variant operation if they exists
+            for collection in operation_collections:
+                svoe_coll = mongo_conn[db_name][collection]
+                hash_to_delete = [op.get('_id') for op in svoe_coll.find(
+                    {'accession': {'$in': sve_accessions}, 'inactiveObjects.seq': assembly_to_delete},
+                    {'_id': '1'})]
+                if hash_to_delete:
+                    delete_result = svoe_coll.delete_many({'_id': {'$in': hash_to_delete}})
+                    logger.info(f"Deleted {delete_result.deleted_count} documents from {collection}.")
+
+            # delete clustered variants if they are orphan
+            cve_accession_to_keep = set()
+            for collection in submitted_collections:
+                sve_coll = mongo_conn[db_name][collection]
+                cve_accession_to_keep.update(set([variant.get('rs') for variant in sve_coll.find({'rs': {'$in': cve_accessions}, 'seq': assembly_to_delete})] ))
+            cve_accessions_to_delete = cve_accessions - cve_accession_to_keep
+            for collection in cluster_collections:
+                cve_coll = mongo_conn[db_name][collection]
+                hash_to_delete = [cluster.get('_id') for cluster in cve_coll.find({'accession': {'$in': cve_accessions_to_delete}}, {'_id': '1'} )]
+                if hash_to_delete:
+                    cve_coll.delete_many({'_id': {'$in': hash_to_delete}})
+                    logger.info(f"Deleted {delete_result.deleted_count} documents from {collection}.")
+
+def main():
+    arg_parse = ArgumentParser(description='Delete variants remapped to  RS')
+    arg_parse.add_argument('--private-config-xml-file',
+                           help="Maven configuration file where connection to mongodb can be found", required=True)
+    arg_parse.add_argument('--profile', help='e.g. production, development or local', required=True)
+
+    args = arg_parse.parse_args()
+
+    # delete variant in the remapped assembly
+    delete_variants(args.private_config_xml_file, args.profile, source_collection='submittedVariantEntity')
+    delete_variants(args.private_config_xml_file, args.profile, source_collection='dbsnpSubmittedVariantEntity')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
